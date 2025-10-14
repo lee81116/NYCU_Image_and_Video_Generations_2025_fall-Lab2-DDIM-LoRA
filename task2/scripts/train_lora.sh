806052418bff6348614d428065b2b5c953af5366
Nian-Chia <103125276+lee81116@users.noreply.github.com>
2025-10-14 16:00:18 +0800

    lora train for colab

--- task2/scripts/train_lora.sh
+++ task2/scripts/train_lora.sh
@@ -1,6 +1,6 @@
 export MODEL_NAME="CompVis/stable-diffusion-v1-4"
-export DATASET_NAME="lambdalabs/naruto-blip-captions"
-export OUTPUT_DIR="./runs/sd-naruto-model-lora"
+export DATASET_NAME="PhanAnh/LOR_art"
+export OUTPUT_DIR="/content/drive/MyDrive/lab2_outputs"
 
 accelerate launch --mixed_precision="no" train_lora.py \
   --pretrained_model_name_or_path=$MODEL_NAME \
