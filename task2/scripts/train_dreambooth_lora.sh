6c1488543d8ece177f66df2d01955d28f7e4c29b
Nian-Chia <103125276+lee81116@users.noreply.github.com>
2025-10-15 00:14:35 +0800

    Update train_dreambooth_lora.sh

--- task2/scripts/train_dreambooth_lora.sh
+++ task2/scripts/train_dreambooth_lora.sh
@@ -1,12 +1,12 @@
 export MODEL_NAME="CompVis/stable-diffusion-v1-4"
-export INSTANCE_DIR="./sample_data/dreambooth-cat"
-export OUTPUT_DIR="./runs/dreambooth_cat"
+export INSTANCE_DIR="/content/drive/MyDrive/lab2_outputs/snowman"
+export OUTPUT_DIR="/content/drive/MyDrive/lab2_outputs/Lora3/dreamboothsnowman"
 
 accelerate launch --mixed_precision="no" train_dreambooth_lora.py \
   --pretrained_model_name_or_path=$MODEL_NAME \
   --instance_data_dir=$INSTANCE_DIR \
   --output_dir=$OUTPUT_DIR \
-  --instance_prompt="a photo of a sks cat" \
+  --instance_prompt="a photo of a sks snowman" \
   --resolution=512 \
   --train_batch_size=1 \
   --gradient_accumulation_steps=1 \
@@ -15,7 +15,7 @@ accelerate launch --mixed_precision="no" train_dreambooth_lora.py \
   --lr_scheduler="constant" \
   --lr_warmup_steps=0 \
   --max_train_steps=500 \
-  --validation_prompt="A photo of sks cat in a bucket" \
+  --validation_prompt="A photo of sks snowman in a pool" \
   --validation_epochs=50 \
   --checkpoints_total_limit 2 \
   --seed="0"
